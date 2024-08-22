7d84597ca41e7dc373ecfafd976a9f70654b74e3
ProfessorThierry <161246655+ProfessorThierry@users.noreply.github.com>
2024-08-22 18:40:04 -0300

    02

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//arquivo para início da Reunião 7
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -8,37 +6,71 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            "Alternativa 1", 
-            "Alternativa 2"
+        enunciado: "Como você acredita que podemos combater o racismo na sociedade?",
+        alternativas: [           
+            {
+                texto: "Participando de campanhas de conscientização e educação sobre igualdade racial.",
+                afirmacao: "Você acredita que a conscientização e a educação são ferramentas essenciais para promover a igualdade racial e combater o racismo."
+            },
+            {
+                texto:  "Denunciando atos de discriminação e apoiando políticas públicas inclusivas.",
+                afirmacao: "Você considera a denúncia de discriminação e o suporte a políticas públicas inclusivas como métodos importantes para enfrentar o racismo."
+            }
         ]
     },
     {
-        enunciado: "Pergunta 2",
-        alternativas: [
-            "Alternativa 1", 
-            "Alternativa 2"
+        enunciado: "Como você pode ajudar a combater a disseminação de fake news, especialmente as que perpetuam o racismo?",
+        alternativas: [ 
+            {
+                texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
+                afirmacao: "Você valoriza a verificação de informações como um meio fundamental para combater a disseminação de fake news e evitar a propagação de conteúdos prejudiciais."
+            },
+            {
+                texto: "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo.",
+                afirmacao: "Você acredita na importância de educar os outros sobre os riscos das fake news e incentivar a responsabilidade ao compartilhar informações, especialmente sobre temas raciais."
+            }
+        ]
+    },
+    {
+        enunciado: "Como podemos incentivar o uso de energia solar e outras formas de energia limpa, especialmente em comunidades carentes?",
+        alternativas: [           
+            {
+                texto: "Promovendo a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.",
+                afirmacao: "Você acredita que a instalação de painéis solares em locais estratégicos como escolas e centros comunitários pode ser uma forma eficaz de promover o uso de energia limpa e beneficiar comunidades carentes."
+            },
+            {
+                texto: "Defendendo políticas públicas que subsidiem a energia solar para famílias de baixa renda.",
+                afirmacao: "Você considera essencial a defesa de políticas públicas que ofereçam subsídios para energia solar, facilitando o acesso a essa tecnologia para famílias de baixa renda."
+            }
         ]
     } 
 ];
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntas[atual].enunciado;
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas){
+    for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas)
     }
 }
 
-mostraPergunta();
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++;
+    mostraPergunta();
+}
+
+mostraPergunta(); 
